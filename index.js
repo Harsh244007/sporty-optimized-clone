cfab6af65f9cb4d5a398d898a30c5d72380293d4
Harsh Patel <Patelharsh241999@gmail.com>
2024-02-27 01:53:33 +0530

    * Test commit .

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ app.get("/api/patron/country/locate", (req, res) => {
     res.send(locateData)
 })
 
+
+
+app.get("/api/news/favorite/list", (req, res) => {
+    res.send([])
+})
+
+
 app.get("/middle/feed/trends", (req, res) => {
     res.send(trendsData)
 })
@@ -24,6 +31,16 @@ app.get("/middle/feed/videos", (req, res) => {
     res.send(videosData)
 })
 app.get("/middle/feed/latest", (req, res) => {
+    res.send(latestData)
+})
+
+app.get("/api/middle/feed/trends", (req, res) => {
+    res.send(trendsData)
+})
+app.get("/api/middle/feed/videos", (req, res) => {
+    res.send(videosData)
+})
+app.get("/api/middle/feed/latest", (req, res) => {
     res.send(latestData)
 })
 
@@ -37,9 +54,6 @@ app.get("/api/sms-center/captcha/config/info", (req, res) => {
 
 
 
-app.get("/", (e, p) => {
-    p.sendFile(path.join(__dirname, "/index.html"));
-});
 
 app.get("/favicon.ico", (e, p) => {
     p.sendFile(path.join(__dirname, "/favicon.ico"));
@@ -48,4 +62,7 @@ app.use("/css", express.static(__dirname + '/css'));
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/fonts", express.static(__dirname + '/fonts'));
 
+app.get("*", (e, p) => {
+    p.sendFile(path.join(__dirname, "/index.html"));
+});
 app.listen(port, () => { })
